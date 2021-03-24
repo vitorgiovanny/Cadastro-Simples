98fd1d69b4b8fe3c2b44ccffe9d6621ba45a4027
vitorgiovanny <vitorgiovannyinvis@gmail.com>
2021-03-24 19:28:29 -0300

    Removendo interfaces IFisica e IJuridica. Criando Novos Metodos. Modificando code Main.

--- src/main/java/com/simples/cadastro/cadastrosimples/Cadastro/Cadastro.java
+++ src/main/java/com/simples/cadastro/cadastrosimples/Cadastro/Cadastro.java
@@ -1,9 +1,8 @@
 
 package com.simples.cadastro.cadastrosimples.Cadastro;
 
-import com.simples.cadastro.interfaces.iFisica;
+
 import com.simples.cadastro.interfaces.iPessoa;
-import com.simples.cadastro.interfaces.iJuridica;
 import java.util.Date;
 import org.apache.commons.lang3.StringUtils;
 
@@ -11,55 +10,60 @@ import org.apache.commons.lang3.StringUtils;
  *
  * @author vitor
  */
-public class Cadastro implements iPessoa, iFisica, iJuridica {
+public class Cadastro implements iPessoa {
     
     private String name;
     private String cpf;
     private String cnpj;
     private Date dataOfBirth;
-    
+
     public Cadastro(Date date){
         this.name = "";
         this.dataOfBirth = date;
     }
     
-    public String filtrarCpfCnpj(String value){
-        if(value.length()<15){
-            String cpf_mask;
-            String cnpj_mask;
-            
-        if(value.length() == 11){
-            this.setCpf(value);
-            cpf_mask = StringUtils.left(value, 3) + 
-                    "." + StringUtils.left((value.substring(value.length()-8)), 3)
-                    + "." + StringUtils.left(value.substring(value.length()-5), 3)
-                    +"-" + StringUtils.right(value, 2);
-            return cpf_mask;
-        }
-        this.setCnpj(value);
-        cnpj_mask = StringUtils.left(value, 2) + 
-                    "." + StringUtils.left((value.substring(value.length()-12)), 3)
-                    + "/" + StringUtils.left(value.substring(value.length()-6), 4)
-                    +"-" + StringUtils.right(value, 2);
-        return cnpj_mask;
-        }
-        
-        return null;
-    }
-
-    public void cadastrar(String nome, String cpf_cnpj, Date date){
+    public void cadastrarPessoaCpf(String nome, String cpf, Date date){
         this.setNome(nome);
-        this.filtrarCpfCnpj(cpf_cnpj);
         this.setDataNascimento(date);
-   
-        if(this.filtrarCpfCnpj(cpf_cnpj).length()==11){
-            this.setCpf(cpf_cnpj);
-        }
-        if(this.filtrarCpfCnpj(cpf_cnpj).length()<15){
-            this.setCnpj(cpf_cnpj);
+        if(this.verificarCpf(cpf) != null){
+            this.setCpf(cpf);
         }
     }
     
+    
+    public void cadastrarPessoaCnpj(String nome, String cnpj, Date date){
+        this.setNome(nome);
+        this.setDataNascimento(date);
+        
+        if(this.verificarCnpj(cnpj) != null){
+            this.setCnpj(cnpj);
+        }
+    }
+
+    public String verificarCpf(String cpf){
+            String cpf_mask;
+            if(cpf.length() == 11){
+                this.setCpf(cpf);
+                cpf_mask = StringUtils.left(cpf, 3) + 
+                        "." + StringUtils.left((cpf.substring(cpf.length()-8)), 3)
+                        + "." + StringUtils.left(cpf.substring(cpf.length()-5), 3)
+                        +"-" + StringUtils.right(cpf, 2);
+                return cpf_mask;
+            }
+            return null;
+    }
+    
+    public String verificarCnpj(String cnpj){
+        String cnpj_mask;
+        if(cnpj.length()<15){
+        cnpj_mask = StringUtils.left(cnpj, 2) + 
+                    "." + StringUtils.left((cnpj.substring(cnpj.length()-12)), 3)
+                    + "/" + StringUtils.left(cnpj.substring(cnpj.length()-6), 4)
+                    +"-" + StringUtils.right(cnpj, 2);
+        return cnpj_mask;
+    }
+        return null;
+    }
     @Override
     public void setNome(String nome) {
         this.name = nome;
@@ -90,12 +94,10 @@ public class Cadastro implements iPessoa, iFisica, iJuridica {
         return this.cpf;
     }
 
-    @Override
     public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
     }
 
-    @Override
     public String getCnpj() {
        return this.cnpj;
     }
